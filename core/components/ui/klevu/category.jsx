9fd7e22fda29f7e9aed91cf0510051e9a7e3887f
Juan Pena <jerrypena1@users.noreply.github.com>
2024-11-13 15:27:43 -0500

    adding addtocart button handler

--- core/components/ui/klevu/category.jsx
+++ core/components/ui/klevu/category.jsx
@@ -4,11 +4,16 @@ import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
 import { KlevuMerchandising } from '@klevu/ui-react';
 import React from 'react';
 
+// import { getProduct } from '../../../app/[locale]/(default)/product/[slug]/page-data';
+import { addToCart } from '../../product-card/add-to-cart/form/_actions/add-to-cart';
+
+import { useCart } from '~/components/header/cart-provider';
 import { KlevuInitWrapper } from '~/components/ui/klevu/init';
 
 
 
 export default function KlevuCategory({ category }) {
+  const cart = useCart();
   const catBreadcrumbs = removeEdgesAndNodes(category.breadcrumbs);
   const getCategoryPath = () => {
     try {
@@ -28,12 +33,44 @@ export default function KlevuCategory({ category }) {
     }
   }
 
+  const handleSubmit = async (product) => {
+    // event.preventDefault();
+
+    const formData = new FormData();
+
+    // this code only works server-side :(
+
+    // const productData = await getProduct({
+    //   entityId: product.id,
+    //   optionValueIds: {},
+    //   useDefaultOptionSelections: true,
+    // });
+
+    formData.append('product_id', product.id);
+    formData.append('quantity', 1);
+    // formData.append('data', productData);
+
+
+
+    // Optimistic update
+    cart.increment(1);
+
+    const result = await addToCart(formData);
+
+    console.log(result);
+  };
+
+
+
+
   return (
     <KlevuInitWrapper>
       <KlevuMerchandising
         category={getCategoryPath()}
         categoryTitle={category.name}
+        onKlevuAddToCart={handleSubmit}
       />
     </KlevuInitWrapper>
+
   );
 };
